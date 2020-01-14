ddfb46b77a2f0e1d42b2fb613cd1d85dc2789ede
Jacek Wieczorek <mrjjot@gmail.com>
2020-01-14 21:22:19 +0100

    Remove license headers (notice will be added in the README)

--- src/com.c
+++ src/com.c
@@ -1,19 +1,3 @@
-/*
-	avrsynth - simple AVR synthesizer project
-	Copyright (C) 2019 Jacek Wieczorek <mrjjot@gmail.com>
-	This file is part of liblightmodbus.
-	Avrsynth is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	Avrsynth is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <avr/io.h>
 #include <inttypes.h>
 #include "com.h"
@@ -22,10 +6,10 @@
 void cominit( uint32_t baud )
 {
 	baud = F_CPU / 16 / baud - 1;
-	
+
 	UBRRH = (unsigned char) ( baud >> 8 ); //Set baud rate
     UBRRL = (unsigned char) baud;
-    
+
     UCSRB = ( 1 << RXEN ) | ( 1 << TXEN ); //Enable RX and TX
     UCSRC = ( 1 << URSEL ) | ( 0 << USBS ) | ( 3 << UCSZ0 ); //Set data format - 8 bit data, 1 stop
 }
--- src/midi.c
+++ src/midi.c
@@ -1,19 +1,3 @@
-/*
-	avrsynth - simple AVR synthesizer project
-	Copyright (C) 2019 Jacek Wieczorek <mrjjot@gmail.com>
-	This file is part of liblightmodbus.
-	Avrsynth is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	Avrsynth is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <stddef.h>
 #include <inttypes.h>
 #include "midi.h"
@@ -21,18 +5,18 @@
 void midiproc( struct midistatus *midi, uint8_t byte, uint8_t channel )
 {
 	uint8_t dlim, dcnt, status;
-	
+
 	//Null pointer check
 	if ( midi == NULL ) return;
-	
+
 	//Get configuration from the struct
 	dlim = midi->dlim;
 	dcnt = midi->dcnt;
 	status = midi->status;
-	
+
 	//Handle synthesizer reset
 	if ( byte == 0xff ) midi->reset = 1;
-	
+
 	if ( byte & ( 1 << 7 ) ) //Handle status bytes
 	{
 		//Extract information from status byte
@@ -47,39 +31,39 @@ void midiproc( struct midistatus *midi, uint8_t byte, uint8_t channel )
 			case 0x10:
 				dlim = 2;
 				break;
-				
+
 			//Note off
 			case 0x00:
 				dlim = 2;
 				break;
-			
+
 			//Controller change
 			case 0x30:
 				dlim = 2;
 				break;
-			
+
 			//Program change
 			case 0x40:
 				dlim = 1;
 				break;
-				
+
 			//Pitch
 			case 0x60:
 				dlim = 2;
 				break;
-			
+
 			//Uknown command
 			default:
 				dlim = 0;
 				break;
-				
+
 		}
 	}
 	else if ( midi->channel == channel ) //Handle data bytes
 	{
 		//Data byte
 		midi->dbuf[dcnt++] = byte;
-		
+
 		//Interpret command
 		if ( dcnt >= dlim )
 		{
@@ -91,38 +75,38 @@ void midiproc( struct midistatus *midi, uint8_t byte, uint8_t channel )
 					midi->notevel = midi->dbuf[1];
 					midi->noteon = 1;
 					break;
-					
-				//Note off	
+
+				//Note off
 				case 0x00:
 					if ( midi->note == midi->dbuf[0] )
 						midi->noteon = 0;
 					break;
-					
+
 				//Controller change
 				case 0x30:
 					midi->controllers.raw[midi->dbuf[0]] = midi->dbuf[1];
 					break;
-									
+
 				//Program change
 				case 0x40:
 					midi->program = midi->dbuf[0];
 					break;
-					
+
 				//Pitch
 				case 0x60:
 					midi->pitchbend = midi->dbuf[0] | ( midi->dbuf[1] << 7 );
 					break;
-					
+
 
 				default:
 					break;
 			}
-			
+
 			dcnt = 0;
 		}
-		
+
 	}
-	
+
 	//Write config back to the struct
 	midi->dlim = dlim;
 	midi->dcnt = dcnt;
